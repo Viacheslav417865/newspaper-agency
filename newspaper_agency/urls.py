b937894764e5c205f67d5b5297bd177275e28d08
Viacheslav417865 <sfinks417865@gmail.com>
2024-11-13 11:32:23 +0200

    add modified .gitignore urls.py & create migrations

--- newspaper_agency/urls.py
+++ newspaper_agency/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("catalog.urls", namespace="catalog")),
     path("accounts/", include("django.contrib.auth.urls")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
